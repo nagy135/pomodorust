114b19f72862dedc01fa6ccde9d15a900ee74d81
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-03-10 13:19:46 +0100

    feat: show time

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-use druid::widget::{Align, Flex, Label, TextBox};
-use druid::{AppLauncher, Data, Env, Lens, LocalizedString, Widget, WidgetExt, WindowDesc};
+use druid::widget::{Align, Flex, Label};
+use druid::{AppLauncher, Data, Env, Lens, LocalizedString, Widget, WindowDesc};
+use std::thread::sleep;
+use std::time::Duration;
 
-const VERTICAL_WIDGET_SPACING: f64 = 20.0;
-const TEXT_BOX_WIDTH: f64 = 200.0;
-const WINDOW_TITLE: LocalizedString<HelloState> = LocalizedString::new("Pomodoro");
+const WINDOW_TITLE: LocalizedString<TimerState> = LocalizedString::new("Pomodoro");
 
 #[derive(Clone, Data, Lens)]
-struct HelloState {
-    name: String,
+struct TimerState {
+    timer: i32,
+    work_limit: i32,
+    rest_limit: i32,
+    work: bool,
 }
 
 fn main() {
@@ -17,31 +20,37 @@ fn main() {
         .window_size((400.0, 400.0));
 
     // create the initial app state
-    let initial_state = HelloState {
-        name: "World".into(),
+    let mut initial_state = TimerState {
+        timer: 267,
+        work_limit: 25 * 60,
+        rest_limit: 5 * 60,
+        work: true,
     };
 
     // start the application
     AppLauncher::with_window(main_window)
         .launch(initial_state)
         .expect("Failed to launch application");
+
+    sleep(Duration::new(2, 0));
+    initial_state.timer = 388;
 }
 
-fn build_root_widget() -> impl Widget<HelloState> {
+fn build_root_widget() -> impl Widget<TimerState> {
     // a label that will determine its text based on the current app data.
-    let label = Label::new(|data: &HelloState, _env: &Env| format!("Hello {}!", data.name));
-    // a textbox that modifies `name`.
-    let textbox = TextBox::new()
-        .with_placeholder("Who are we greeting?")
-        .fix_width(TEXT_BOX_WIDTH)
-        .lens(HelloState::name);
+    let label = Label::new(|data: &TimerState, _env: &Env| {
+        format!("Hello {}!", number_to_time(data.timer))
+    });
 
     // arrange the two widgets vertically, with some padding
-    let layout = Flex::column()
-        .with_child(label)
-        .with_spacer(VERTICAL_WIDGET_SPACING)
-        .with_child(textbox);
+    let layout = Flex::column().with_child(label);
 
     // center the two widgets in the available space
     Align::centered(layout)
+}
+
+fn number_to_time(number: i32) -> String {
+    let minutes = number / 60;
+    let seconds = number % 60;
+    format!("{}:{}", minutes, seconds)
 }
