cab7395af62d46c9368b711f57847724a8fb4e86
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-03-12 20:44:26 +0100

    feat: Settings + WindowSettings + Text(demo)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
-use iced::{slider, Column, Element, ProgressBar, Sandbox, Settings, Slider};
+use iced::window::Settings as WindowSettings;
+use iced::{
+    slider, Column, Element, HorizontalAlignment, Length, ProgressBar, Sandbox, Settings, Slider,
+    Text,
+};
 
 pub fn main() -> iced::Result {
-    Progress::run(Settings::default())
+    Pomodoro::run(Settings {
+        window: WindowSettings {
+            size: (400, 400),
+            ..WindowSettings::default()
+        },
+        ..Settings::default()
+    })
 }
 
 #[derive(Default)]
-struct Progress {
+struct Pomodoro {
     value: f32,
     progress_bar_slider: slider::State,
 }
@@ -15,7 +25,7 @@ enum Message {
     SliderChanged(f32),
 }
 
-impl Sandbox for Progress {
+impl Sandbox for Pomodoro {
     type Message = Message;
 
     fn new() -> Self {
@@ -33,9 +43,16 @@ impl Sandbox for Progress {
     }
 
     fn view(&mut self) -> Element<Message> {
+        let title = Text::new("Counter")
+            .width(Length::Fill)
+            .size(100)
+            .color([0.5, 0.5, 0.5])
+            .horizontal_alignment(HorizontalAlignment::Center);
+
         Column::new()
             .padding(20)
             .push(ProgressBar::new(0.0..=100.0, self.value))
+            .push(title)
             .push(
                 Slider::new(
                     &mut self.progress_bar_slider,
